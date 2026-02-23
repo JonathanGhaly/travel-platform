84ecdb1817fdee2efb288c6768cc1a240d007b62
JonathanGhaly <jghaly00@gmail.com>
2026-02-24 00:09:42 +0200

    create travel-users-service

---
+++ init-db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE travel_poi;
+CREATE DATABASE travel_booking;
+CREATE DATABASE travel_users;
+CREATE DATABASE travel_auth;
+CREATE DATABASE travel_itinerary;
+CREATE DATABASE travel_notification;
